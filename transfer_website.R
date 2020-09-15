8f004e6adb6ef87e4af6c9e409967de7566473cc
Cody Dailey <daileyco@uga.edu>
2020-09-15 18:12:22 -0400

    edits

--- transfer_website.R
+++ transfer_website.R
@@ -9,5 +9,5 @@ website.files <- list.files("./public", full.names = TRUE)
 
 dir.create("./docs")
 docs.folder <- file.path("./docs")
-file.copy(website.files, github.website, recursive = TRUE, overwrite = TRUE)
+file.copy(website.files, docs.folder, recursive = TRUE, overwrite = TRUE)
 file.create("./docs/.nojekyll")
